5be7d034382bdc17acac673caa01b7c31bacb3dc
Gustavo Mori <gustavo.mori.peixoto@usp.br>
2020-08-03 16:34:25 -0300

    style of template (mobile/web)

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // Icons and images from assets
 import logoImg from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg'
-import studyIcon from '../../assets/images/icons/study.svg'
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+// Css from styles Landing
+import './styles.css';
 
 function Landing() {
     return (
