21192617ad8741fd637246e8cbf40ddf398ec68f
Gustavo Mori <gustavo.mori.peixoto@usp.br>
2020-08-05 17:01:39 -0300

    add inputs component and styles of signup

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
 
-
 // Components
 import PageHeader from '../../componentes/PageHeader';
 import TeacherItem from '../../componentes/TeacherItem'
+import Input from '../../componentes/input';
+// Style
 import './styles.css';
-
 
 function TeacherList() {
     return(
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="week_day" label="Dia da semana"/>
+                    <Input type="time" name="time" label="Hora"/>
                 </form>
             </PageHeader>
             <main>
